1320da5015277d77b5e05fb6cd05915ed60b9239
nlysenko <nikolai.v.lysenko@gmail.com>
2022-01-26 20:44:51 +0300

    Remove custom button from notifications screen

--- components/@triplr/mobile/src/notifications/components/fragments/Notifications/index.tsx
+++ components/@triplr/mobile/src/notifications/components/fragments/Notifications/index.tsx
@@ -5,21 +5,13 @@
  */
 
 import { View, Text } from 'react-native'
-import { RootStackParamList } from '@/app/navigation/RootStack'
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-
-import { BackButton } from '@/app/components/BackButton'
 
 import { styles } from './styles'
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Notifications'>
-
-export function Notifications({ navigation }: Props) {
+export function Notifications() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Notifications</Text>
-
-      <BackButton onButtonPress={() => navigation.goBack()} />
     </View>
   )
 }
